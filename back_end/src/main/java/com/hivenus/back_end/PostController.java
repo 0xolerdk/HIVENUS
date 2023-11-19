3e81b4c268e2d0a692059593d6c072f1c3a8551c
Oleh Fihol <43319066+0xolerdk@users.noreply.github.com>
2023-11-20 00:14:35 +0100

    postCont

--- back_end/src/main/java/com/hivenus/back_end/PostController.java
+++ back_end/src/main/java/com/hivenus/back_end/PostController.java
@@ -1,2 +1,17 @@
-package com.hivenus.back_end;public class PostController {
-}
+//package com.hivenus.back_end;
+//
+//
+//import jakarta.servlet.http.HttpServletResponse;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.io.IOException;
+//
+//@RestController
+//public class PostController {
+//
+//    @RequestMapping(value = "/")
+//    public void redirect(HttpServletResponse response) throws IOException {
+//        response.sendRedirect("/swagger-ui.html");
+//    }
+//}
