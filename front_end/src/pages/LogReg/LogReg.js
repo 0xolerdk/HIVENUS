def217de56759db30c578342981491b853a7cd7c
Oleh Fihol <43319066+0xolerdk@users.noreply.github.com>
2024-04-14 15:39:25 +0200

    LogReg fix

--- front_end/src/pages/LogReg/LogReg.js
+++ front_end/src/pages/LogReg/LogReg.js
@@ -1,20 +1,25 @@
 import Bottom from '../../components/Bottom.js';
+import back_button from '../../assets/images/back_button.svg'
+import Home from '../Home/Home.js'
 import './LogReg.css';
+import { BrowserRouter as Link } from 'react-router-dom';
+
 
 
 function LogReg() {
     return (
       <div className="LogReg">
         <div className="top">
-          <h1>Welcome to Our Website!</h1>
-          <p>Please sign in or sign up to continue.</p>
           <div className="buttons">
+            
+            {/* <Link className="back_button" to="/"><a href="">
+            <img  src={back_button} alt="" />
+            </a></Link> */}
             <button className="sign-in">Sign In</button>
             <button className="sign-up">Sign Up</button>
           </div>
         </div>
             <Bottom />
-
       </div>
     );
   }
