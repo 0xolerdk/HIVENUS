334d56c9b9009cae8db739745034420dd050c1d2
Oleh Fihol <43319066+0xolerdk@users.noreply.github.com>
2024-05-23 19:23:18 +0300

    new Top_Bar

--- front_end/src/components/Top_Bar/Top_Bar.js
+++ front_end/src/components/Top_Bar/Top_Bar.js
@@ -1,23 +1,64 @@
-import './Top_Bar.css';
+import "./Top_Bar.css";
 import logo from "../../assets/images/logo.svg";
-import Add from './Add';
+import Add from "./Add";
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import SettingsIcon from '@mui/icons-material/Settings';
+import DataUsageIcon from '@mui/icons-material/DataUsage';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/system';
+
+const TabsContainer = styled(Box)({
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr 1fr',
+  width: '100%',
+  alignItems: 'center',
+});
+
+const CenteredTab = styled(Tab)({
+  gridColumn: '2 / 3',
+  justifySelf: 'center',
+});
+
+const RightAlignedTab = styled(Tab)({
+  gridColumn: '3 / 4',
+  justifySelf: 'end',
+});
+
 
 function Top_Bar() {
-    return (
-        <div className="bar"> 
-            
-            <div className="menu">
-                <img className='logo' src={logo} alt="" />
-                <button className="menu-button">Statistic</button>
+  const [value, setValue] = React.useState(0);
 
-                <button className="menu-button">Settings</button>
-                <Add/>
-                
-                
-            </div>
-            
-        </div>
-    );
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="bar">
+      <div className="tabs">
+      <img className="logo" src={logo} alt="" />
+      <Tabs
+      value={value}
+      onChange={handleChange}
+      aria-label="icon tabs example"
+      style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}
+    >
+      <Tab 
+        icon={<DataUsageIcon />} 
+        aria-label="dashboard" 
+        style={{  marginRight: 'auto', left: "50%",
+        transform:" translateX(-50%)"}}
+      />
+      <Tab 
+        icon={<SettingsIcon  />} 
+        aria-label="settings" 
+        style={{ marginLeft: 'auto' }}
+      />
+    </Tabs>
+      </div>
+    </div>
+  );
 }
 
 export default Top_Bar;
