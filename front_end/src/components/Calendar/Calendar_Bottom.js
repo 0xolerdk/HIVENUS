06cbfd97e9d64489fd517b1d09920ec9ae432218
Oleh Fihol <43319066+0xolerdk@users.noreply.github.com>
2024-05-26 01:41:59 +0300

    Redesign of donuts

--- front_end/src/components/Calendar/Calendar_Bottom.js
+++ front_end/src/components/Calendar/Calendar_Bottom.js
@@ -3,41 +3,59 @@ import React, { useState, useEffect  } from "react";
 import dayjs from "dayjs";
 
 const data = {
-  labels: ["Green", "Blue", "Pink", "Yellow", "Grey"],
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
   datasets: [
     {
-      data: [10, 50, 50, 20, 10],
-      backgroundColor: ["#4CAF50", "#00BCD4", "#E91E63", "#FFC107", "#9E9E9E"],
+      label: 'Dataset 1',
+      data: [90,10],
+      backgroundColor: [
+        '#4caf50',
+        '#333333'
+
+
+      ],
+      borderWidth: 1.2,
+      borderColor: "#333333",
+      borderRadius: 10
     },
-  ],
+    {
+      label: 'Dataset 2',
+      data: [60,40],
+      backgroundColor: [
+        '#00bcd4',
+        '#333333'
+
+
+      ],
+      borderWidth: 1,
+      borderColor: "#333333",
+      borderRadius: 10
+    },
+    {
+      label: 'Dataset 3',
+      data: [70,30],
+      backgroundColor: [
+        '#e91e63',
+        '#333333'
+
+
+      ],
+      borderWidth: 1.5,
+      borderColor: "#333333",
+      borderRadius: 10
+    }
+  ]
 };
 
-const data2 = {
-  labels: ["Green", "Blue", "Pink", "Yellow", "Grey"],
-  datasets: [
-    {
-      data: [15, 10, 20, 30, 60],
-      backgroundColor: ["#4CAF50", "#00BCD4", "#E91E63", "#FFC107", "#9E9E9E"],
-    },
-  ],
-};
 
-const data3 = {
-  labels: ["Green", "Blue", "Pink", "Yellow", "Grey"],
-  datasets: [
-    {
-      data: [15, 20, 20, 50, 30],
-      backgroundColor: ["#4CAF50", "#00BCD4", "#E91E63", "#FFC107", "#9E9E9E"],
-    },
-  ],
-};
+
 
 const options1 = {
   height: 100,
-  borderWidth: 1,
+  borderWidth: 0,
   fullSize: false,
   radius: 20,
-  cutout: "85%", // This changes the radius of the Doughnut hole.
+  cutout: "50%", // This changes the radius of the Doughnut hole.
   rotation: 90, // This rotates the chart by 90 degrees.
   // Add other options here
 };
@@ -45,7 +63,7 @@ const options1 = {
 const options2 = {
   borderWidth: 1,
   radius: 30,
-  cutout: "85%", // This changes the radius of the Doughnut hole.
+  cutout: "50%", // This changes the radius of the Doughnut hole.
   rotation: 90, // This rotates the chart by 90 degrees.
   // Add other options here
 };
@@ -53,14 +71,14 @@ const options2 = {
 const options3 = {
   borderWidth: 1,
   radius: 40,
-  cutout: "85%", // This changes the radius of the Doughnut hole.
+  cutout: "50%", // This changes the radius of the Doughnut hole.
   rotation: 90, // This rotates the chart by 90 degrees.
   // Add other options here
 };
 const options4 = {
   borderWidth: 1,
   radius: 50,
-  cutout: "85%", // This changes the radius of the Doughnut hole.
+  cutout: "50%", // This changes the radius of the Doughnut hole.
   rotation: 90, // This rotates the chart by 90 degrees.
   // Add other options here
 };
@@ -97,7 +115,7 @@ export default function Calendar_Bottom({ selectedDate, onDateChange }) {
       </div>
       <div onClick={() => subtractDay(2)}>
         <Donut
-          data={data2}
+          data={data}
           options={options2}
           width="100px"
           height="100px"
@@ -107,7 +125,7 @@ export default function Calendar_Bottom({ selectedDate, onDateChange }) {
       <div onClick={() => subtractDay(1)}>
         {" "}
         <Donut
-          data={data3}
+          data={data}
           options={options3}
           width="110px"
           height="110px"
@@ -117,7 +135,7 @@ export default function Calendar_Bottom({ selectedDate, onDateChange }) {
       <div>
         {" "}
         <Donut
-          data={data2}
+          data={data}
           options={options4}
           width="120px"
           height="120px"
@@ -128,7 +146,7 @@ export default function Calendar_Bottom({ selectedDate, onDateChange }) {
       <div onClick={() => addDay(1)}>
         {" "}
         <Donut
-          data={data2}
+          data={data}
           options={options3}
           width="110px"
           height="110px"
@@ -138,7 +156,7 @@ export default function Calendar_Bottom({ selectedDate, onDateChange }) {
       <div onClick={() => addDay(2)}>
         {" "}
         <Donut
-          data={data2}
+          data={data}
           options={options2}
           width="100px"
           height="100px"
@@ -147,7 +165,7 @@ export default function Calendar_Bottom({ selectedDate, onDateChange }) {
       </div>
       <div onClick={() => addDay(3)}>
         <Donut
-          data={data2}
+          data={data}
           options={options1}
           width="60px"
           height="60px"
--- front_end/src/components/Statistics.js
+++ front_end/src/components/Statistics.js
@@ -3,78 +3,74 @@ import "./Statistics.css"
 import Donut from './Donut';
 
 const data = {
-  labels: ['Green', 'Blue', 'Pink', 'Yellow', 'Grey'],
+  labels: ['Red', 'Blue'],
   datasets: [
     {
-      data: [10, 50, 50, 20, 10],
-      backgroundColor: ['#4CAF50', '#00BCD4', '#E91E63', '#FFC107', '#9E9E9E'],
+      label: 'Dataset 1',
+      data: [90,10],
+      backgroundColor: [
+        '#4caf50',
+        '#333333'
+
+
+      ],
+      borderWidth: 5,
+      borderColor: "#333333",
+      borderRadius: 10
     },
-  ],
+    {
+      label: 'Dataset 2',
+      data: [60,40],
+      backgroundColor: [
+        '#00bcd4',
+        '#333333'
+
+
+      ],
+      borderWidth: 4,
+      borderColor: "#333333",
+      borderRadius: 10
+    },
+    {
+      label: 'Dataset 3',
+      data: [70,30],
+      backgroundColor: [
+        '#e91e63',
+        '#333333'
+
+
+      ],
+      borderWidth: 3,
+      borderColor: "#333333",
+      borderRadius: 10,
+      hoverOffset: 4
+    }
+  ]
 };
 
-const data2 = {
-  labels: ['Green', 'Blue', 'Pink', 'Yellow', 'Grey'],
-  datasets: [
-    {
-      data: [15, 10, 20, 30, 60],
-      backgroundColor: ['#4CAF50', '#00BCD4', '#E91E63', '#FFC107', '#9E9E9E'],
-    },
-  ],
-};
 
-const data3 = {
-  labels: ['Green', 'Blue', 'Pink', 'Yellow', 'Grey'],
-  datasets: [
-    {
-      data: [15, 20, 20, 50, 30],
-      backgroundColor: ['#4CAF50', '#00BCD4', '#E91E63', '#FFC107', '#9E9E9E'],
-    },
-  ],
-};
 
 const options1 = {
-    height: 100,
+    height: 120,
     borderWidth: 1,
     fullSize: false,
-    radius: 120,
-    cutout: '85%', // This changes the radius of the Doughnut hole.
-    rotation: 90, // This rotates the chart by 90 degrees.
+    radius: 140,
+    cutout: '60%', // This changes the radius of the Doughnut hole.
+    rotation: 0, // This rotates the chart by 90 degrees.
     // Add other options here
   };
   
-  const options2 = {
-    borderWidth: 1,
-    radius: 120,
-    cutout: '85%', // This changes the radius of the Doughnut hole.
-    rotation: 90, // This rotates the chart by 90 degrees.
-    // Add other options here
-  };
-
-  const options3 = {
-    borderWidth: 1,
-    radius: 120,
-    cutout: '85%', // This changes the radius of the Doughnut hole.
-    rotation: 90, // This rotates the chart by 90 degrees.
-    // Add other options here
-  };
-  const options4 = {
-    borderWidth: 1,
-    radius: 50,
-    cutout: '85%', // This changes the radius of the Doughnut hole.
-    rotation: 90, // This rotates the chart by 90 degrees.
-    // Add other options here
-  };
 
 function Calendar() {
     return (
         
         <div className="statistics"> 
         <div className='row'><Donut  data={data} options={options1} text="Food Intake" link="/calories_intake" font_size={1.7}/>
-            <Donut data={data2} options={options2} text="Activity "/>
-            <Donut data={data3} options={options3} text="Calories Burned" font_size={1.7}/></div>
+            <Donut data={data} options={options1} text="Activity "/>
+            <Donut data={data} options={options1} text="Calories Burned" font_size={1.4}/></div>
         <div className='row'><Donut data={data} options={options1} text="Sleep" link="/sleep_track"/>
-            <Donut data={data2} options={options2} text="Water Intake" font_size={1.7}/>
-            <Donut data={data3} options={options3} text="Weight"/>
+            <Donut data={data} options={options1} text="Water Intake" font_size={1.7}/>
+            <Donut data={data} options={options1} text="Weight"/>
           </div>    
             
         </div>
